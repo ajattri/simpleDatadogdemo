7ce2aabe1d248c114bc49628150e2d3e7f05e74f
ajay kumar <ajay.kumar@galaxyweblinks.in>
2023-10-06 12:36:25 +0530

    setting up datadog upload sourcemap during build to ci/cp pipeline

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,25 @@
+import { datadogRum } from '@datadog/browser-rum';
+import getConfig from 'next/config';
+
+const {publicRuntimeConfig} = getConfig()
+const {dataDogBrowserConfig} = publicRuntimeConfig
+
+datadogRum.init({
+    debug:true,
+    applicationId: dataDogBrowserConfig.applicationId,
+    clientToken: dataDogBrowserConfig.clientToken,
+    site: 'us5.datadoghq.com',
+    service:'simpledemo',
+    env:'prod',
+    version: publicRuntimeConfig.appVersion, 
+    sessionSampleRate:100,
+    premiumSampleRate: 100,
+    trackUserInteractions: true,
+    defaultPrivacyLevel:'mask-user-input'
+});
+    
+datadogRum.startSessionReplayRecording();
+
 function HomePage() {
   return (
     <div>
