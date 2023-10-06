838981d806e31b122457da12ed52bb3cf08a23a7
ajay kumar <ajay.kumar@galaxyweblinks.in>
2023-10-06 14:31:44 +0530

    create scripts for uploadSourcemaps and deleteSourcemaps

---
+++ deleteSourcemaps.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+function deleteSourcemaps(directory) {
+  const files = fs.readdirSync(directory);
+  for (const file of files) {
+    const fullPath = path.join(directory, file);
+    if (file.endsWith('.map')) {
+      fs.unlinkSync(fullPath);
+    } else if (fs.statSync(fullPath).isDirectory()) {
+      deleteSourcemaps(fullPath);
+    }
+  }
+}
+
+deleteSourcemaps('./.next');
