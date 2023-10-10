b41f34eb9ad7a84f459fe903a1aa21ea3c97b365
ajattri <147041225+ajattri@users.noreply.github.com>
2023-10-10 08:19:35 +0000

    Deploying to gh-pages from @ ajattri/simpleDatadogdemo@2c6282096a0c6128cf5e7df631399e166c93d199 🚀

---
+++ _next/static/wUGJ-ZvnTJNt5ZlfMUzdN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/692-916e21b3edd3646b.js","static/chunks/pages/index-b647ac9528904b2e.js"],"/_error":["static/chunks/pages/_error-962110356089be88.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
